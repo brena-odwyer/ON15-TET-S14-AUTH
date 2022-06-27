d4f790dde330bf6fea3211def91c0828fd9423b8
brena-odwyer <odwyer.brena@gmail.com>
2022-06-27 12:51:25 -0300

    anotacoes aula

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,44 @@
 const UserSchema = require('../models/userSchema');
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
 
 const login = (req, res) => {
-    
+    try {
+        // UserSchema.findOne(filtro é o email do usuario, função anonima) essas são os parâmetros
+        UserSchema.findOne({ email: req.body.email }, (error, user) => {
+            if(!user) {
+                return res.status(401).send({
+                    message: "User não encontrado",
+                    email: `${req.body.email}`
+                })
+            }
+            // o que eu tenho: usuário da requisição e usuário do bando de dados, mesmo email
+            // preciso verificar se é também a mesma senha
+            //chamar o bcrypt, passar a função de comparar; o req.body.password é a requisição e o user.password é o que vem do banco
+            //usamos o password pq corresponde lá no Schema
+            const validPassword = bcrypt.compareSync(req.body.password, user.password);
+            
+            if(!validPassword) {
+                return res.status(401).send({
+                    "message": "Login não autorizado"
+                })
+              };
+            // jwt.sign(recebe dois, parâmetros)
+            //jwt.sign(nome do usuário, SECRET)
+            //aí guardamos isso numa const chamada de token
+            const token = jwt.sign({ name: user.name }, SECRET);
+            
+            res.status(200).send({
+                message: "Login autorizado",
+                token
+            });
+        
+        });
+        
+    } catch (error) {
+        console.error(error)
+    };
 };
 
 module.exports = {
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,22 @@
-const UserSchema = require("../models/userSchema");
+const userSchema = require("../models/userSchema");
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const SECRET = process.env.SECRET;
 
 const getAll = async (req, res) => {
+  const authHeader = req.get('authorization')
+  const token = authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).send("Erro no header")
+  }
+
+  jwt.verify(token, SECRET, (err) => {
+    if(err) {
+        return res.status(401).send("Não autorizado")
+    }
+})
+    
   UserSchema.find(function (err, users) {
     if(err) {
       res.status(500).send({ message: err.message })
@@ -9,6 +25,45 @@ const getAll = async (req, res) => {
   }) 
 };
 
-module.exports = {
-  getAll
+
+//getAll sem autorização
+// const getAll = async (req, res) => {
+//   userSchema.find(function (err, users) {
+//     if(err) {
+//       res.status(500).send({ message: err.message })
+//     }
+//       res.status(200).send(users)
+//   }) 
+// };
+
+const createUser = async (req, res) => {
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = hashedPassword;
+
+  try {
+    if(!req.body.name || !req.body.email) {
+      res.status(404).send({
+        "message": "Campos obrigatórios precisam ser preenchidos"
+      })
+    };
+    const newUser = new userSchema ({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+  
+    const savedUser = await newUser.save();
+
+    res.status(201).send({
+      "message": "Usuário criado com sucesso",
+      savedUser
+    });
+  } catch (e) {
+    console.log(e)
+  };
 };
+
+module.exports = {
+  getAll,
+  createUser
+}
